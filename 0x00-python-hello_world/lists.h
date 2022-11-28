cc709eb7265ea44920a5263e63b53593759031ee
oladelebasheer <ajaoooladele@gmail.com>
2022-11-28 12:00:21 -0800

    first commit

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,17 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_list(const listint_t *h);
+listint_t *add_nodeint(list_t **head, const int n);
+void free_listint(listint_t * head);
+int check_cycle(listint_t *list);
+
+#endif /* LIST_H */
