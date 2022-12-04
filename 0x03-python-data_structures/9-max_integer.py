ca208ea653cada489d32e71c7777a991679209dc
oladelebasheer <ajaoooladele@gmail.com>
2022-12-04 03:46:31 -0800

    Task 10

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        amax = my_list[0]
-        for a in my_list:
-            if a > imax:
-                amax = a
-        return amax
+        imax = my_list[0]
+        for i in my_list:
+            if i > imax:
+                imax = i
+        return imax
     return None
