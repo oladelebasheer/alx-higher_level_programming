a2c6f6c2a0cb34ccc5a2c38e9f1c05c75e5e6c73
oladelebasheer <ajaoooladele@gmail.com>
2022-12-21 15:22:47 -0800

    Task 1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+"""Square generation module for Python project 0x06"""
 class Square:
-    """Defines a square"""
+    """class defined for square generation"""
     def __init__(self, size):
-        """Initialises the data"""
         self.__size = size
