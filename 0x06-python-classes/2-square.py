595512e8efc5e52e53814ff6c06269fcff472157
oladelebasheer <ajaoooladele@gmail.com>
2022-12-21 15:32:07 -0800

    Task 2. Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-"""Square generation module for Python project 0x06"""
 class Square:
-    """class defined for square generation"""
-    def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-
-        self.__size = 
+    """Defines a square"""
+    def __init__(self, size):
+        """Initialises the data"""
+        self.__size = size
