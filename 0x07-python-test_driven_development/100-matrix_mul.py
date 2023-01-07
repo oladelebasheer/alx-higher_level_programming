0fbadaabb3c14f87c2908ce6ca711aa5385c754f
oladelebasheer <ajaoooladele@gmail.com>
2023-01-06 16:30:06 -0800

    Update Task 6. Matrix multiplication

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -6,7 +6,7 @@ Multiplies 2 matrices by using 'numpy' module.
 import numpy as lazy
 
 
-def lazy_matrix_mul(m_a, m_b):
+def matrix_mul(m_a, m_b):
     """
     Function to multiplies 2 matrices.
     m_a: As a list of lists.
