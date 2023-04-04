8d0b3d574dcfe5a27137db27d10d64719ef7b9b3
oladelebasheer <ajaoooladele@gmail.com>
2023-04-04 08:23:05 -0700

    Update task 3. cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL and displays all HTTP methods the server will accept
-curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-`
+#  a Bash script that takes in a URL and displays all HTTP methods the server will accept
+curl -si "$1" | awk -F ": " '/Allow/ {print $2}'
